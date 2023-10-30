c4ee86e1d712ae5ee63a0804d35a671f4d78e30f
brayanospina2005 <ospinabrayan431@gmail.com>
2023-10-30 14:45:52 -0500

    scroll reveal, responsive, animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,51 @@ const blurHeader =  () => {
                         : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader);
+
+/* show scroll up */
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
+
+/* scroll section active link */
+
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+             sectionTop = current.offsetTop - 58,
+             sectionId = current.getAttribute('id'),
+             sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+
+        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link')
+                                                                    : document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove('active-link')
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+/* scroll reveal animation */
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    delay: 200,
+    // reset: true
+})
+
+/* sr.reveal(`.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content`, {
+    origin: 'top',
+    interval: 100
+}) */
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
+sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval:100})
+sr.reveal(`.about__data, .join__image`, {origin: 'right'})
+sr.reveal(`.about__image, .join__data`, {origin: 'left'})
+sr.reveal(`.popular__card`, {interval:200})
