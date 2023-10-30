201d170e82e162a4e60802921c1aa9ebb6cfdf07
brayanospina2005 <ospinabrayan431@gmail.com>
2023-10-30 14:48:04 -0500

    lasttttt

--- assets/js/app.js
+++ assets/js/app.js
@@ -67,7 +67,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 3000,
     delay: 400,
-    // reset: true
+    reset: true
 })
 
 /* sr.reveal(`.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content`, {
